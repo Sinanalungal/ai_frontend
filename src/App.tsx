6db47828fe672b99c849b403c54e264125b54e3f
muhamed sinan <Sinan@viscandiagnostics.com>
2025-01-18 16:56:30 +0530

    bugs solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import PageRoutes from './Routes/pageRouters'
 import './App.css'
 import { Toaster } from "sonner";
+// import { X } from 'lucide-react';
 
 
 function App() {
@@ -15,14 +16,14 @@ function App() {
               classNames: {
                 closeButton: "p-1 hover:bg-gray-100 rounded-full"
               },
-              closeButton: ({ toast }: any) => (
-                <button
-                  onClick={() => toast.dismiss()}
-                  className="p-1 hover:bg-gray-100 rounded-full"
-                >
-                  <X className="h-4 w-4 text-gray-500" />
-                </button>
-              )
+              // closeButton: ({ toast }: any) => (
+              //   <button
+              //     onClick={() => toast.dismiss()}
+              //     className="p-1 hover:bg-gray-100 rounded-full"
+              //   >
+              //     <X className="h-4 w-4 text-gray-500" />
+              //   </button>
+              // )
             }}
           />
       <PageRoutes />
